48bb12bbf7a5364a9064d6a9c904ac2b7007f09b
santhosh-000 <santhoshkumarat2004@gmail.com>
2026-02-02 15:17:04 +0530

    Fix CORS for Netlify frontend

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "https://yourfrontend.netlify.app" }));
+app.use(cors({ origin: "https://mellow-marzipan-1405af.netlify.app/" }));
 
  
 
